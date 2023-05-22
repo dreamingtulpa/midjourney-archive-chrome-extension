9589a21054de4946101ce07b3c6618e873c98cd4
GanWeaving <ganweaving@fastmail.com>
2023-05-22 11:33:21 +0200

    update

--- src/popup_new.js
+++ src/popup_new.js
@@ -1,6 +1,4 @@
-import JSzip from "jszip";
-
-let fileCount = 0;
+import JSZip from "jszip";
 
 document.getElementById("dateForm").addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -9,6 +7,7 @@ document.getElementById("dateForm").addEventListener("submit", async (event) => 
   const endDate = new Date(event.target.endDate.value);
   const upscaleSelection = document.querySelector('input[name="upscaleOptions"]:checked').value;
 
+  // Show progress container and hide form
   const progressContainer = document.getElementById("progressContainer");
   const progressDaysBar = document.getElementById("progressDaysBar");
   const progressDaysMessage = document.getElementById("progressDaysMessage");
@@ -21,25 +20,31 @@ document.getElementById("dateForm").addEventListener("submit", async (event) => 
   let processedDays = 0;
   const totalDays = Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24)) + 1;
 
+  // Iterate through the dates and perform API calls
   for (let currentDate = startDate; currentDate <= endDate; currentDate.setDate(currentDate.getDate() + 1)) {
     progressDaysMessage.innerText = `[${processedDays}/${totalDays}] Processing ${currentDate.toISOString().slice(0, 10)}...`;
     const year = currentDate.getFullYear();
     const month = currentDate.getMonth() + 1;
     const day = currentDate.getDate();
 
+    // Fetch the archive data
     const archiveResponse = await fetch(`https://www.midjourney.com/api/app/archive/day/?day=${day}&month=${month}&year=${year}`);
     const archiveData = await archiveResponse.json();
 
-    const zip = new JSzip();
-    let csvData = "Filename,Prompt\n"; // Define CSV headers
-    //let fileCount = 0;
+    // Create a zip file for the current date
+    const zip = new JSZip();
+    let fileCount = 0;
+
     let processedJobs = 0;
     let totalJobs = archiveData.length;
 
+    // Process each item in the archive data
     for (const item of archiveData) {
+      // const jobId = item.id;
       const jobId = item;
       progressJobsMessage.innerText = `[${processedJobs}/${totalJobs}] Fetching ${jobId}...`;
 
+      // Fetch the job status data
       const jobStatusResponse = await fetch("https://www.midjourney.com/api/app/job-status/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -48,37 +53,31 @@ document.getElementById("dateForm").addEventListener("submit", async (event) => 
       const jobStatusData = await jobStatusResponse.json();
       console.log(jobStatusData);
 
+      // Check job version
       let versionNumber = parseFloat(jobStatusData._parsed_params.version);
       let isVersion5Plus = !isNaN(versionNumber) && versionNumber >= 5;
 
+      // Set event type
       let eventType = jobStatusData.event.eventType
 
-      if (isVersion5Plus && eventType === "imagine" && upscaleSelection === "allImagesV5Grids") {
-        let result = await processImages(jobStatusData, zip, csvData);
-        fileCount += result.fileCount;
-        csvData = result.csvData;
+      // Process images
+      if (isVersion5Plus && eventType !== "upscale" && upscaleSelection === "allImagesV5Grids") {
+        fileCount += await processImages(jobStatusData, zip);
       } else if (isVersion5Plus && eventType === "upscale" && upscaleSelection === "onlyV5Upscales") {
-        let result = await processImages(jobStatusData, zip, csvData);
-        fileCount += result.fileCount;
-        csvData = result.csvData;
+        fileCount += await processImages(jobStatusData, zip);
       } else if (!isVersion5Plus && eventType === "upscale") {
-        let result = await processImages(jobStatusData, zip, csvData);
-        fileCount += result.fileCount;
-        csvData = result.csvData;
+        fileCount += await processImages(jobStatusData, zip);
       }
 
       processedJobs++;
       progressJobsBar.value = (processedJobs / totalJobs) * 100;
       progressJobsMessage.innerText = `[${processedJobs}/${totalJobs}] Fetched ${jobId}!`;
     }
-
-    zip.file("image_data.csv", csvData);
-
     processedDays++;
     progressDaysBar.value = (processedDays / totalDays) * 100;
 
     // Generate and download the zip file
-    if (fileCount > 0) {  
+    if (fileCount > 0) {
       const zipBlob = await zip.generateAsync({ type: "blob" });
       const downloadUrl = URL.createObjectURL(zipBlob);
       const downloadLink = document.createElement("a");
@@ -89,29 +88,28 @@ document.getElementById("dateForm").addEventListener("submit", async (event) => 
       URL.revokeObjectURL(downloadUrl);
     }
   }
-
   progressDaysMessage.innerText = "Download complete!";
 });
 
-async function processImages(jobStatusData, zip, csvData) {
+async function processImages(jobStatusData, zip) {
+  // Download and process images
   const { username, image_paths, id, parent_id, enqueue_time, full_command, prompt, event, _parsed_params } = jobStatusData;
   let fileCount = 0;
 
-  if (!image_paths) {
-    console.error('image_paths is null or undefined');
-    return;
-  }
-
   for (const [index, imagePath] of image_paths.entries()) {
+    // Fetch the image
     const response = await fetch(imagePath);
     const imageBlob = await response.blob();
 
+    // Filename conversions
     const truncated_username = makeFilenameCompatible(username);
     const truncated_prompt = makeFilenameCompatible(prompt, 48);
     const datetime = convertDateTimeFormat(enqueue_time);
 
+    // Add EXIF metadata
     const modifiedBlob = await addExifMetadata(imageBlob, jobStatusData);
 
+    // Build filename
     let filename;
     if (image_paths.length > 1) {
       filename = `${datetime}_${id}_${index}_${truncated_prompt}.png`
@@ -119,14 +117,12 @@ async function processImages(jobStatusData, zip, csvData) {
       filename = `${datetime}_${id}_${truncated_prompt}.png`
     }
 
+    // Add the image to the zip file
     zip.file(filename, modifiedBlob);
     fileCount++;
-
-    // Append data to CSV
-    csvData += `${filename},"${prompt}"\n`;
   }
 
-  return { fileCount: fileCount, csvData: csvData };
+  return fileCount;
 }
 
 function makeFilenameCompatible(str, maxLength) {
@@ -144,7 +140,7 @@ function makeFilenameCompatible(str, maxLength) {
 function convertDateTimeFormat(dateTimeString) {
   const dt = new Date(dateTimeString);
   const year = dt.getFullYear();
-  const month = String(dt.getMonth() + 1).padStart(2, '0');
+  const month = String(dt.getMonth() + 1).padStart(2, '0'); // Months are 0-based in JavaScript
   const day = String(dt.getDate()).padStart(2, '0');
   const hours = String(dt.getHours()).padStart(2, '0');
   const minutes = String(dt.getMinutes()).padStart(2, '0');
